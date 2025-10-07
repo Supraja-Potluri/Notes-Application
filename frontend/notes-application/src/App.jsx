d7b3d6f9b95282ea5459cf51e56685fb71c920de
Supraja-Potluri <potlurisupraja81.com>
2025-10-07 14:58:03 +0530

    Add authentication check on app start to redirect to login or dashboard

--- frontend/notes-application/src/App.jsx
+++ frontend/notes-application/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
 
 const App = () => {
+  const isAuthenticated = !!localStorage.getItem('token');
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<></>} />
+        <Route path="/" element={isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
         <Route path="/dashboard" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
